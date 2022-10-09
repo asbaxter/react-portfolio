a3ce1ffa0192d9ab70fc5b001f6e8ae5d5355e1c
Andrew Baxter <asbaxter1@gmail.com>
2022-10-09 17:11:29 -0400

    added contact section

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -17,7 +17,7 @@ export default function Header(props){
                     <a href="#about" onClick={() => {setIsProjects(false); setIsAbout(true); setIsContact(false); setIsResume(false)}}>About Me</a>
                 </li>
                 <li>
-                    <a href="#portfolio" onClick={() => {setIsProjects(true); setIsAbout(false); setIsContact(false); setIsResume(false)}}>Portfolio</a>
+                    <a href="#projects" onClick={() => {setIsProjects(true); setIsAbout(false); setIsContact(false); setIsResume(false)}}>Projects</a>
                 </li>
                 <li>
                     <a href="#about" onClick={() => {setIsProjects(false); setIsAbout(false); setIsContact(true); setIsResume(false)}}>Contact</a>
