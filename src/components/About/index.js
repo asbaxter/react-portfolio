460aab5c5d8a50e5f49877aaa5c11c433de5e9dd
Andrew Baxter <asbaxter1@gmail.com>
2022-10-09 12:37:24 -0400

    initial commit

---
+++ src/components/About/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function About(){
+    return (
+    <section>
+      <h1>About Me</h1>
+      <div>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sit amet tellus cras adipiscing enim. Nullam ac tortor vitae purus. Vulputate eu scelerisque felis imperdiet proin fermentum. Gravida quis blandit turpis cursus in. Egestas congue quisque egestas diam in arcu cursus euismod. Nunc mi ipsum faucibus vitae aliquet nec ullamcorper. Nisl condimentum id venenatis a condimentum vitae sapien pellentesque. Sagittis purus sit amet volutpat consequat mauris nunc congue nisi. Nunc vel risus commodo viverra maecenas accumsan. Id nibh tortor id aliquet lectus proin nibh nisl condimentum. Sed arcu non odio euismod.
+        </p>
+      </div>
+    </section>
+    )
+}
