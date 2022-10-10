ad9e020963db33262be84a5155102e8039bff535
Andrew Baxter <asbaxter1@gmail.com>
2022-10-09 22:35:30 -0400

    more minor style changes

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -8,22 +8,24 @@ export default function Header(props){
         setIsResume
     } = props;
 
+
+
     return (
         <header>
             <a href="/" id="header-title">Andrew Baxter</a>
             <nav>
             <ul>
                 <li>
-                    <a href="#about" onClick={() => {setIsProjects(false); setIsAbout(true); setIsContact(false); setIsResume(false)}}>About Me</a>
+                    <a href="#about" onClick={() => {setIsProjects(false); setIsAbout(true); setIsContact(false); setIsResume(false);}}>About Me</a>
                 </li>
                 <li>
-                    <a href="#projects" onClick={() => {setIsProjects(true); setIsAbout(false); setIsContact(false); setIsResume(false)}}>Projects</a>
+                    <a href="#projects" onClick={() => {setIsProjects(true); setIsAbout(false); setIsContact(false); setIsResume(false); }}>Projects</a>
                 </li>
                 <li>
-                    <a href="#about" onClick={() => {setIsProjects(false); setIsAbout(false); setIsContact(true); setIsResume(false)}}>Contact</a>
+                    <a href="#about" onClick={() => {setIsProjects(false); setIsAbout(false); setIsContact(true); setIsResume(false); }}>Contact</a>
                 </li>
                 <li>
-                    <a href="#resume" onClick={() => {setIsProjects(false); setIsAbout(false); setIsContact(false); setIsResume(true)}}>Resume</a>
+                    <a href="#resume" onClick={() => {setIsProjects(false); setIsAbout(false); setIsContact(false); setIsResume(true); }}>Resume</a>
                 </li>
             </ul>
         </nav>
